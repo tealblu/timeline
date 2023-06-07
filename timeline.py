1def41affdad1e5beb809ebdbc96a1d19e5d72bf
Charles Hartsell <hah9ad@bosch.com>
2023-06-07 13:17:37 -0400

    persistence works

--- timeline.py
+++ timeline.py
@@ -9,11 +9,15 @@ class Entry:
         self.children = []
 
     def __init__(self, date, content):
-        self.date = datetime.datetime.strptime(date, "%d%m%Y%H%M%S") #ddMMyyyyhhmmss
+        self.date = datetime.datetime.strptime(date, DATE_FORMAT) #ddMMyyyyhhmmss
         self.content = content
         self.children = []
         
     def addChild(self, child):
+        if child.date in (d.date for d in self.children): # check if error already exists as child at same time
+            print("ERROR: ENTRY ALREADY EXISTS")
+            return
+
         self.children.append(child)
         self.children.sort(key=lambda e: e.date)
 
@@ -30,14 +34,16 @@ class Entry:
             contentList.extend(child.listChildren(indent=indent + 2))
         return contentList
 
-    def createTimeline(self, indent=0):
+    def timeline(self, indent=0):
         timeline = " " * indent + self.date.strftime("%d/%m/%Y %H:%M:%S") + ": " + self.content + "\n"
         for child in self.children:
-            timeline += child.createTimeline(indent=indent + 2)
+            timeline += child.timeline(indent=indent + 2)
         return timeline
 
 # Globals
 data_filepath = os.path.dirname(os.path.realpath(__file__)) + "\pickled_data.pkl"
+root = None
+DATE_FORMAT = "%d%m%Y%H%M%S"
 
 def write_data(data):
     with open(data_filepath, 'wb') as outfile:
@@ -47,9 +53,14 @@ def read_data():
     with open(data_filepath, "rb") as infile:
         return pickle.load(infile)
 
-def test():
+def root_init():
+    global root
     root = read_data()
-    if root:
-        print(root.children)
 
-test()
+def root_save():
+    write_data(root)
+
+def main():
+    print("nothing here yet...")
+
+main()
