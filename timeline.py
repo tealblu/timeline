1cc54a9dba9e81608129158a3612d3b611387227
Charles "Blue" Hartsell <98487762+tealblu@users.noreply.github.com>
2023-06-10 22:35:06 -0400

    split into multiple files

--- timeline.py
+++ timeline.py
@@ -1,93 +1,15 @@
-import os
-import datetime
-import pickle as pickle
-import sys
-from flask import *
+# ----- IMPORTS -----
+from flask import *; app = Flask(__name__)
 
-app = Flask(__name__)
+# ----- CUSTOM LIBRARIES -----
+from Entry import * # Defines Entry class and related functions
+from root_util import * # Provides persistence utility via pkl, accessed through root variable
 
-class Entry:
-    def __init__(self):
-        self.date = ""
-        self.content = ""
-        self.children = []
+# ----- GLOBALS -----
+DATE_FORMAT = r"%d%m%Y%H%M%S" # datecode format
+READABLE_DATE = r"%d/%m/%Y %H:%M:%S" # format for printing
 
-    def __init__(self, date, content):
-        self.date = datetime.datetime.strptime(date, DATE_FORMAT) #ddMMyyyyhhmmss
-        self.content = content
-        self.children = []
-        
-    def addChild(self, child):
-        if child.date in (d.date for d in self.children): # check if error already exists as child at same time
-            print("ERROR: ENTRY ALREADY EXISTS")
-            return
-
-        self.children.append(child)
-        self.children.sort(key=lambda e: e.date)
-
-    def removeChild(self, date):
-        # find the child, remove it
-        for child in self.children:
-            if child.date == date:
-                self.children.remove(child)
-
-    def listChildren(self, indent=0):
-        contentList = []
-        for child in self.children:
-            contentList.append(" " * indent + "- " + child.content)
-            contentList.extend(child.listChildren(indent=indent + 2))
-        return contentList
-    
-    def getChildren(self):
-        return self.children
-    
-    def findParent(self, target_date):
-        if any(child.date.strftime(DATE_FORMAT) == target_date for child in self.children):
-            return self
-        else:
-            for child in self.children:
-                parent = child.findParent(target_date)
-                if parent:
-                    return parent
-        print("NO PARENT")
-        return None
-
-    def timeline(self, indent=0):
-        timeline = " " * indent + self.date.strftime(READABLE_DATE) + ": " + self.content + "\n"
-        for child in self.children:
-            timeline += child.timeline(indent=indent + 2)
-        return timeline
-
-def write_data(data):
-    with open(data_filepath, 'wb') as outfile:
-        pickle.dump(data, outfile)
-
-def read_data():
-    with open(data_filepath, "rb") as infile:
-        return pickle.load(infile)
-
-def root_init():
-    global root
-    root = read_data()
-
-def root_save():
-    write_data(root)
-
-def get_entry(entry, datecode):
-    if entry.date.strftime(DATE_FORMAT) == datecode:
-        return entry
-    else:
-        for child in entry.children:
-            result = get_entry(child, datecode)
-            if result:
-                return result
-    return None
-
-# Globals
-data_filepath = os.path.dirname(os.path.realpath(__file__)) + "\pickled_data.pkl"
-root = Entry
-DATE_FORMAT = "%d%m%Y%H%M%S"
-READABLE_DATE = "%d/%m/%Y %H:%M:%S"
+# ----- VIEWS -----
 
 # redirect to root timeline
 @app.route('/')
